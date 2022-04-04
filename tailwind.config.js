5939da94972f50abfdefe8e97bfd0fdddc19ad47
RMo <morici23@gmail.com>
2022-04-03 23:45:48 -0400

    reset git

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        'murica': '#0477B8',
+        'salmon': '#F39189',
+        'smoke': '#F39189',
+        'white': '#FFFFFF'
+      },
+      fontFamily: {
+        'lobster': ['Lobster', 'cursive'],
+      },
+    },
+  },
+  plugins: [],
+}
