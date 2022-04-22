f855344e6908abd97eba35ab7197374577bbfdeb
RMo <morici23@gmail.com>
2022-04-21 21:18:52 -0400

    Stub riffin into portfolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,20 @@
 module.exports = {
-  content: ["./src/index.html"],
+  content: ["./src/index.html", "./src/riffin/index.html"],
   theme: {
     extend: {
       colors: {
         'murica': '#0477B8',
         'salmon': '#F39189',
         'smoke': '#F39189',
-        'white': '#FFFFFF'
+        'white': '#FFFFFF',
+        'nord-0': '#2E3440',
+        'nord-2': '#434C5E',
+        'nord-frost': '#8FBCBB',
       },
       fontFamily: {
         'lobster': ['Lobster', 'cursive'],
+        'roboto-mono': ['Roboto Mono', 'monospace'],
+        'ultra': ['Ultra', 'serif'],
       },
     },
   },
